f2850b37ae20c371ad5b34082f2a7c6eda0bab37
hatredholder <rattlingserpent@gmail.com>
2022-11-16 13:59:46 +0300

    added signup route

--- authentication/__init__.py
+++ authentication/__init__.py
@@ -1,12 +1,15 @@
-from flask import Blueprint, redirect, render_template, url_for
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 
+from werkzeug.security import generate_password_hash
+
+from .models import User
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/login")
 def login():
-    return render_template("index.html")
+    return render_template("authentication/login.html")
 
 
 # @auth.route('/login', methods=['POST'])
@@ -16,14 +19,28 @@ def login():
 
 @auth.route("/signup")
 def signup():
-    return render_template("signup.html")
+    return render_template("authentication/signup.html")
 
 
-# @auth.route('/signup', methods=['POST'])
-# def signup_post():
-#     return render_template('signup.html')
+@auth.route("/signup", methods=["POST"])
+def signup_post():
+    email = request.form.get("email")
+    name = request.form.get("name")
+    password = request.form.get("password")
+
+    user = User.objects(email=email).first()
+
+    # If email registered already - redirect back to signup
+    if user:
+        flash("Email address already exists.")
+        return redirect(url_for("auth.signup"))
+
+    # Add user
+    User(email=email, name=name, password=generate_password_hash(password)).save()
+
+    return redirect(url_for("auth.login"))
 
 
 @auth.route("/logout")
 def logout():
-    return redirect(url_for("main.index"))
+    return redirect(url_for("pastebin.index"))
