10d34fc4034f646d54abc3d6c8b42fd27d44088f
hatredholder <rattlingserpent@gmail.com>
2022-11-16 13:59:21 +0300

    added user model

---
+++ authentication/models.py
@@ -0,0 +1,9 @@
+from app import db
+
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Document):
+    email = db.EmailField(max_length=100, unique=True, required=True)
+    password = db.StringField(max_length=1000, required=True)
+    name = db.StringField(max_length=100, required=True)
