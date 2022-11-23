e6432631a33031e215c957c6c299b4d9c08df20a
hatredholder <rattlingserpent@gmail.com>
2022-11-23 14:52:13 +0300

    added TagListField

---
+++ pastebin/utils.py
@@ -0,0 +1,51 @@
+from wtforms.fields import StringField
+
+
+class TagListField(StringField):
+    """Stringfield for a list of separated tags"""
+
+    def __init__(
+        self,
+        label="",
+        validators=None,
+        remove_duplicates=True,
+        to_lowercase=True,
+        separator=" ",
+        **kwargs,
+    ):
+        """
+        Construct a new field.
+        :param label: The label of the field.
+        :param validators: A sequence of validators to call when validate is called.
+        :param remove_duplicates: Remove duplicates in a case insensitive manner.
+        :param to_lowercase: Cast all values to lowercase.
+        :param separator: The separator that splits the individual tags.
+        """
+        super(TagListField, self).__init__(label, validators, **kwargs)
+        self.remove_duplicates = remove_duplicates
+        self.to_lowercase = to_lowercase
+        self.separator = separator
+        self.data = []
+
+    def _value(self):
+        if self.data:
+            return ", ".join(self.data)
+        else:
+            return ""
+
+    def process_formdata(self, valuelist):
+        if valuelist:
+            self.data = [x.strip() for x in valuelist[0].split(self.separator)]
+            if self.remove_duplicates:
+                self.data = list(self._remove_duplicates(self.data))
+            if self.to_lowercase:
+                self.data = [x.lower() for x in self.data if x != ""]
+
+    @classmethod
+    def _remove_duplicates(cls, seq):
+        """Remove duplicates in a case insensitive, but case preserving manner"""
+        d = {}
+        for item in seq:
+            if item.lower() not in d:
+                d[item.lower()] = True
+                yield item
