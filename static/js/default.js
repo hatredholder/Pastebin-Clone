ea19023df079186616f7155fc7bc5f9f82362a96
hatredholder <rattlingserpent@gmail.com>
2022-12-19 21:59:03 +0300

    updated jquery script to change to selected syntax option

--- static/js/default.js
+++ static/js/default.js
@@ -1,8 +1,15 @@
-$(document).ready(function(){
-	var code = $(".codemirror-textarea")[0];
-	var editor = CodeMirror.fromTextArea(code, {
-		lineNumbers : false,
-    mode: "javascript",
-    theme: "material-darker"
-	});
+var code = $(".codemirror-textarea")[0];
+var editor = CodeMirror.fromTextArea(code, {
+  lineNumbers : false,
+  mode: "None",
+  theme: "material-darker"
 });
+$('select').on('change', function (e) {
+    var cm = $(".CodeMirror")[0];
+  	var code = $(".codemirror-textarea")[0];
+
+    var valueSelected = this.value;
+
+    editor.setOption("mode", valueSelected);
+});
+
