4bb0aa599db7f53b7b5ef1bc63541c3e5bbe0859
hatredholder <rattlingserpent@gmail.com>
2022-12-24 16:21:57 +0300

    added on/off button for syntax highlighting

--- static/js/default.js
+++ static/js/default.js
@@ -9,6 +9,7 @@ var editor = CodeMirror.fromTextArea(code, {
 // cdnjs.com available language modes
 available = ["apl", "asciiarmor", "asn.1", "asterisk", "brainfuck", "clike", "clojure", "cmake", "cobol", "coffeescript", "commonlisp", "crystal", "css", "cypher", "d", "dart", "diff", "django", "dockerfile", "dtd", "dylan", "ebnf", "ecl", "eiffel", "elm", "erlang", "factor", "fcl", "forth", "fortran", "gas", "gfm", "gherkin", "go", "groovy", "haml", "handlebars", "haskell-literate", "haskell", "haxe", "htmlembedded", "htmlmixed", "http", "idl", "javascript", "jinja2", "jsxx", "julia", "livescript", "lua", "markdown", "mathematica", "mbox", "mirc", "mllike", "modelica", "mscgen", "mumps", "nginx", "nsis", "ntriples", "octave", "oz", "pascal", "pe", "perl", "php", "pig", "powershell", "properties", "protobuf", "pug", "puppet", "python", "q", "r", "rpm", "rst", "ruby", "rust", "sas", "sass", "scheme", "shell", "sieve", "slim", "smalltalk", "smarty", "solr", "soy", "sparql", "spreadsheet", "sql", "stex", "stylus", "swift", "tcl", "textile", "tiddlywiki", "tiki", "toml", "tornado", "troff", "ttcn-cfg", "ttcn", "turtle", "twig", "vb", "vbscript", "velocity", "verilog", "vhdl", "vue", "wast", "webidl", "xml", "xquery", "yacas", "yaml-frontmatter", "yaml", "z80"]
 
+
 // On selected option
 $('select').on('change', function (e) {
     var cm = $(".CodeMirror")[0];
@@ -33,6 +34,16 @@ $('select').on('change', function (e) {
             editor.setOption("mode", valueSelected);
         }      
       }
+    }
+});
+
+// On/off toggle button
+$('#onoff').on('change', function() {
+    if (this.value == "off"){
+      document.getElementById("onoff").value="on"
+    }
+    else {
+      document.getElementById("onoff").value="off"
     }
 });
 
