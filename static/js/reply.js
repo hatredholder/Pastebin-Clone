6d1aee38c6c6e2dbed9d28b4e3cca064c5004682
hatredholder <rattlingserpent@gmail.com>
2023-01-01 21:57:52 +0300

    updated js comments

--- static/js/reply.js
+++ static/js/reply.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   // Hide "Add A Comment" button on page load
   $(".add-a-comment").hide()
 
-  // Move form on reply button click
+  // Move CommentForm on reply button click
   $(".reply-btn").click(function() {
       var id = this.id
       $(`.reply-container-${id}`).append($(".add_comment_form"))
@@ -11,7 +11,7 @@ $(document).ready(function() {
       $(".add-a-comment").show()
   });
 
-  // Move form to its original place on "Add A Comment" button click and hide it
+  // Move CommentForm to its original place on "Add A Comment" button click and hide the button
   $(".add-a-comment").click(function() {
       var id = this.id
       $(`.reply-container-${id}`).append($(".add_comment_form"))
