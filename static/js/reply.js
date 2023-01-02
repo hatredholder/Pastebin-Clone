b27c0ff1c48c730edce4ccf8a215fd06a493d151
hatredholder <rattlingserpent@gmail.com>
2023-01-02 19:37:14 +0300

    renamed form container and updated replies

--- static/js/reply.js
+++ static/js/reply.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   // Move CommentForm on reply button click
   $(".reply-btn").click(function() {
       var id = this.id
-      $(`.reply-container-${id}`).append($(".add_comment_form"))
+      $(`.reply-form-container-${id}`).append($(".add_comment_form"))
       $(".add_comment_form").find('form').attr("action", `/${id}/`)
       $(".add-a-comment").show()
   });
@@ -14,7 +14,7 @@ $(document).ready(function() {
   // Move CommentForm to its original place on "Add A Comment" button click and hide the button
   $(".add-a-comment").click(function() {
       var id = this.id
-      $(`.reply-container-${id}`).append($(".add_comment_form"))
+      $(`.reply-form-container-${id}`).append($(".add_comment_form"))
       $(".add_comment_form").find('form').attr("action", `/${id}/`)
       $(".add-a-comment").hide()
   })
