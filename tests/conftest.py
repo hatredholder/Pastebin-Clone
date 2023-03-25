3b513ffea61b4ee571b85b019d8789d7ef631a17
hatredholder <rattlingserpent@gmail.com>
2023-03-25 20:59:52 +0300

    fixed app fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-from flask import Flask
 
 from flask_mongoengine import MongoEngine
 
@@ -7,13 +6,12 @@ import mongoengine
 import pytest
 
 
-@pytest.fixture()
+@pytest.fixture
 def app():
     """
     Setup Flask app for testing
     """
-
-    app = Flask(__name__)
+    from app import app
 
     app.config["TESTING"] = True  # enable testing
     app.config["WTF_CSRF_ENABLED"] = False  # disable csrf for forms
@@ -21,11 +19,12 @@ def app():
     mongoengine.connection.disconnect_all()
 
     # Use app_context
+    # (https://flask.palletsprojects.com/en/2.2.x/testing/#tests-that-depend-on-an-active-context)
     with app.app_context():
         yield app
 
 
-@pytest.fixture()
+@pytest.fixture
 def db(app):
     """
     Setup Mongoengine Database
@@ -42,6 +41,9 @@ def db(app):
 
     # Setup db MongoEngine variable
     db = MongoEngine(app)
+
+    # Teardown (clear database before tests)
+    db.connection["default"].drop_database("pastebin_test_db")
 
     # Send the variable
     yield db
